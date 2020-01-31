861c5368c8b05c23ce09f8dfb7304088420f1455
AnthonyTB <anthonylol@protonmail.com>
2020-01-31 08:25:13 -0800

    'restructure'

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import Home from './Routes/Home';
+import Adoption from './Routes/Adoption';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Switch>
+        <Route
+          exact
+          path='/'
+          render={renderProps => {
+            return <Home {...renderProps} />;
+          }}
+        />
+        <Route
+          exact
+          path='/Adoption'
+          render={renderProps => {
+            return <Adoption {...renderProps} />;
+          }}
+        />
+      </Switch>
     </div>
   );
 }
---
+++ src/Routes/Adoption.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Adoption extends React.Component {
+  render() {
+    return (
+      <div className='Adoption'>
+        <h1>Adoption Process</h1>
+      </div>
+    );
+  }
+}
