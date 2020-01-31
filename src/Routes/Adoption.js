5c77c471bd1e5ea2af9ab584a4879368e11a4a06
Mason Reichbauer <mreichba@mail.usf.edu>
2020-01-31 12:04:47 -0500

    setting up adoption page

--- src/Routes/Adoption.js
+++ src/Routes/Adoption.js
@@ -1,10 +1,41 @@
 import React from 'react';
+import './Adoption.css';
 
 export default class Adoption extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      dogs: [],
+      cats: [],
+    };
+  }
+  getDogs = () => {
+
+  }
+
+  getCats = () => {
+
+  }
+
   render() {
     return (
       <div className='Adoption'>
         <h1>Adoption Process</h1>
+        <div className='dog'>
+          <h2>Dogs</h2>
+          dog info here
+        </div>
+
+        <div className='cat'>
+          <h2>Cats</h2>
+          cat info here
+        </div>
+
+        <div>
+          <h3>Place in line</h3>
+          queue info goes here
+        </div>
       </div>
     );
   }
