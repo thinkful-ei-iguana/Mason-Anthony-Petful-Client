13116cb300bb2fd8eab8241a0f57605494c4ba4b
Mason Reichbauer <mreichba@mail.usf.edu>
2020-01-31 12:43:02 -0500

    added code for showing cats and dog info

--- src/Routes/Adoption.js
+++ src/Routes/Adoption.js
@@ -6,36 +6,55 @@ export default class Adoption extends React.Component {
     super(props);
     this.state = {
       error: null,
-      dogs: [],
-      cats: [],
+      dog: [],
+      cat: [],
     };
   }
   componentDidMount() {
-    this.getDogs();
-    this.getCats();
+    this.getDog();
+    this.getCat();
   }
 
 
-  getDogs = () => {
+  getDog = () => {
 
   }
 
-  getCats = () => {
+  getCat = () => {
 
   }
 
   render() {
+    const dog = this.state.dog;
+    const cat = this.state.cat;
+
     return (
       <div className='Adoption'>
         <h1>Adoption Process</h1>
         <div className='dog'>
           <h2>Dogs</h2>
-          dog info here
+          <p>
+            <span className='bold'>  Name: </span>{dog.name}
+            <span className='bold'>  Breed: </span>{dog.breed}
+            <span className='bold'>  Age: </span>{dog.age}
+            <span className='bold'>  Sex: </span>{dog.sex}
+            <span className='bold'>  Description: </span>{dog.description}
+            <span className='bold'>  Story: </span>{dog.story}
+          </p>
+          <button>Adopt Dog</button>
         </div>
 
         <div className='cat'>
           <h2>Cats</h2>
-          cat info here
+          <p>
+            <span className='bold'>  Name: </span>{cat.name}
+            <span className='bold'>  Breed: </span>{cat.breed}
+            <span className='bold'>  Age: </span>{cat.age}
+            <span className='bold'>  Sex: </span>{cat.sex}
+            <span className='bold'>  Description: </span>{cat.description}
+            <span className='bold'>  Story: </span>{cat.story}
+          </p>
+          <button>Adopt Cat</button>
         </div>
 
         <div>
