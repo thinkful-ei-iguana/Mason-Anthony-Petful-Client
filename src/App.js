d8053a4c0f0251edcc57908a0011762e7e9d7228
Mason Reichbauer <mreichba@mail.usf.edu>
2020-01-31 11:36:13 -0500

    working

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import Home from './Routes/Home';
 import Adoption from './Routes/Adoption';
--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import './App.css';
+
+
 
 class Home extends React.Component {
   constructor(props) {
