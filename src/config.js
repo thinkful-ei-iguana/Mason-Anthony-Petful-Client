0bb3031b9bd80cf0115f433947d493facdf0eb3d
AnthonyTB <anthonylol@protonmail.com>
2020-01-31 23:01:37 -0800

    deployed

--- src/config.js
+++ src/config.js
@@ -1,3 +1,5 @@
 export default {
-  REACT_APP_API_BASE: process.env.REACT_APP_API_BASE || 'http://localhost:8080/api'
-}
+  REACT_APP_API_BASE:
+    process.env.REACT_APP_API_BASE ||
+    'https://agile-river-99145.herokuapp.com/api'
+};
